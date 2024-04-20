008a6979da4fffa9521881339fc99c2c0d2fd0ee
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-04-21 00:57:55 +0300

    Update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,49 +2,34 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
-const fulfilledCheckbox = document.querySelector('input[value="fulfilled"]');
-const rejectedCheckbox = document.querySelector('input[value="rejected"]');
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const delay = form.elements.delay.value * 1000;
+  const delay = form.elements.delay.value;
+  const state = form.elements.state.value;
 
-  if (fulfilledCheckbox.checked) {
-    const promiseFulfilled = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(
-          iziToast.success({
-            message: `✅ Fulfilled promise in ${delay}ms`,
-          })
-        );
-      }, delay);
-    })
-      .then(message => {
-        console.log(message);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
 
-  if (rejectedCheckbox.checked) {
-    const promiseRejected = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject(
-          iziToast.error({
-            message: `❌ Rejected promise in ${delay}ms`,
-          })
-        );
-      }, delay);
-    })
-      .then(message => {
-        console.log(message);
-      })
-      .catch(error => {
-        console.error(error);
+  promise
+    .then(() => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
       });
-  }
+    })
+    .catch(() => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
 }
