8215af22dc93f2ae1e639595f9c4ba8fe04612f9
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-04-21 10:42:03 +0300

    Changed radio state

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -8,12 +8,19 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const delay = form.elements.delay.value;
-  const state = form.elements.state.value;
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state;
+  let stateValue;
+
+  state.forEach(radio => {
+    if (radio.checked) {
+      stateValue = radio.value;
+    }
+  });
 
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (state === 'fulfilled') {
+      if (stateValue === 'fulfilled') {
         resolve();
       } else {
         reject();
